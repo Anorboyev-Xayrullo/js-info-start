e1807da1009cac2bb24512da4e89650ac72d9662
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-03-27 12:36:45 +0500

    fix

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 
 
-let text = document.querySelectorAll("h1")
-let button = document.querySelector("button")
+// let text = document.querySelectorAll("h1")
+// let button = document.querySelector("button")
 
 
-// console.log(button);
-console.log(text);
+// // console.log(button);
+// console.log(text);
 
-button.onclick =() =>{
-for(let i = 0 ; i <= text.length -1; i++){
-      text[i].style.color = "red"
-      text[i].style.textAlign= "center"
-    }
-    console.log(text[i]);
+// button.onclick =() =>{
+// for(let i = 0 ; i <= text.length -1; i++){
+//       text[i].style.color = "red"
+//       text[i].style.textAlign= "center"
+//     }
+//     console.log(text[i]);
+// }
+
+
+
+
+
+// let a = +prompt("son kiriting");
+
+// if (a % 2 === 0) {
+//   console.log("juft " ,  a);
+// }else if (a % 2 == 1){
+//   console.log("toq", a);
+// }else{
+//   console.log("bunday amal yo'q");
+// }
+
+
+let a = document.querySelector("input")
+let b = document.querySelector("button")
+
+// console.log(a);
+// console.log(b);
+
+
+
+b.onclick = () => {
+  // if (a.value === 1234 ){
+  //   console.log("parol to'gri");
+  // }else if (a.value == 2134){
+  //   console.log("parol notogri");
+  // }else {
+  //   console.log("bunday narsa yoq");
+  // }
+  console.log(a);
 }
+
+
+
+
+
 
